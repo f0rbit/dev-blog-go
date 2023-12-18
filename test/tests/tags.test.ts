6410b3051cf99327854ca7572b0276f87a1fa0ca
f0rbit <dev@forbit.dev>
2023-12-18 16:47:23 +1030

    begin work on tags testing

---
+++ test/tests/tags.test.ts
@@ -0,0 +1,64 @@
+import { beforeAll, expect, test, describe } from "bun:test";
+
+const test_post = {
+    slug: 'tag-test-post',
+    title: "Tag Test Post",
+    content: "this post should be removed after tests complete.",
+    category: "root"
+}
+let test_id = null as number | null;
+
+const tags = ["tag-1", "tag-2", "tag-3"] as const;
+
+beforeAll(async () => {
+    // insert a fresh post so we have something to test against
+    const response = await fetch("localhost:8080/post/new", { method: "POST", body: JSON.stringify(test_post) });
+    expect(response).toBeTruthy();
+    const result = (await response.json()) as any;
+    expect(result.id).toBeTruthy();
+    expect(result.slug).toBe(test_post.slug);
+    test_id = result.id;
+    console.log(test_id);
+});
+
+describe("tags", () => {
+    test("add", async () => {
+        const response = await fetch(`localhost:8080/post/tag?id=${test_id}&tag=test-tag`, { method: "PUT" });
+        expect(response).toBeTruthy();
+        expect(response.ok).toBeTrue();
+    })
+    test("add duplicate", async () => {
+        const response = await fetch(`localhost:8080/post/tag?id=${test_id}&tag=test-tag`, { method: "PUT" });
+        expect(response).toBeTruthy();
+        expect(response.ok).toBeFalse();
+    })
+    test("add multiple", async () => {
+        for (const tag of tags) {
+            const response = await fetch(`localhost:8080/post/tag?id=${test_id}&tag=${tag}`, { method: "PUT" });
+            expect(response).toBeTruthy();
+            expect(response.ok).toBeTrue();
+        }
+    })
+    test("get tags", async () => {
+        const response = await fetch("localhost:8080/tags", { method: "GET" });
+        expect(response).toBeTruthy();
+        expect(response.ok).toBeTrue();
+        const result = await response.json() as string[];
+        for (const tag of tags) {
+            expect(result).toContain(tag)
+        }
+        expect(result).toContain("test-tag");
+    })
+    test("get tagged posts", async () => {
+        /** @todo implement after backend is done */
+    })
+    test("delete tag", async () => {
+        const response = await fetch(`localhost:8080/post/tag?id=${test_id}&tag=test-tag`, { method: "DELETE" });
+        expect(response).toBeTruthy();
+        expect(response.ok).toBeTrue();
+        // there should no longer be a "test-tag" in the tags
+        const check = await fetch("localhost:8080/tags", { method: "GET" });
+        const check_result = await check.json();
+        expect(check_result).not.toContain("test-tag");
+    })
+})
