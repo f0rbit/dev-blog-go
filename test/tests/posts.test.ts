ac08799e7c1acbbc7fd0799d1fc69043d37873a8
f0rbit <dev@forbit.dev>
2023-12-18 10:35:16 +1030

    fix expects to include toBe functions & implement posts test routes

--- test/tests/posts.test.ts
+++ test/tests/posts.test.ts
@@ -1,5 +1,4 @@
 import { expect, test, describe, afterAll } from "bun:test";
-import { exit } from "process";
 
 type Category = {
     name: string,
@@ -32,53 +31,122 @@ const test_post = {
 }
 let test_post_id: number | null = null;
 
+const pagination_posts = [
+    { slug: "pagepost-1", title: "Page Post 1", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-2", title: "Page Post 2", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-3", title: "Page Post 3", content: "Lorem Ipsum", category: "learning" },
+    { slug: "pagepost-4", title: "Page Post 4", content: "Lorem Ipsum", category: "devlog" },
+    { slug: "pagepost-5", title: "Page Post 5", content: "Lorem Ipsum", category: "devlog" },
+    { slug: "pagepost-6", title: "Page Post 6", content: "Lorem Ipsum", category: "gamedev" },
+    { slug: "pagepost-7", title: "Page Post 7", content: "Lorem Ipsum", category: "gamedev" },
+    { slug: "pagepost-8", title: "Page Post 8", content: "Lorem Ipsum", category: "hobbies" },
+    { slug: "pagepost-9", title: "Page Post 9", content: "Lorem Ipsum", category: "hobbies" },
+    { slug: "pagepost-10", title: "Page Post 10", content: "Lorem Ipsum", category: "story" },
+    { slug: "pagepost-11", title: "Page Post 11", content: "Lorem Ipsum", category: "learning" },
+    { slug: "pagepost-12", title: "Page Post 12", content: "Lorem Ipsum", category: "gamedev" },
+    { slug: "pagepost-13", title: "Page Post 13", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-14", title: "Page Post 14", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-15", title: "Page Post 15", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-16", title: "Page Post 16", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-17", title: "Page Post 17", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-18", title: "Page Post 18", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-19", title: "Page Post 19", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-20", title: "Page Post 20", content: "Lorem Ipsum", category: "coding" },
+    { slug: "pagepost-21", title: "Page Post 21", content: "Lorem Ipsum", category: "coding" },
+];
+const pagination_ids = new Map<string, number>();
+
 describe("posts", () => {
     describe("simple operations", () => {
         test("create", async () => {
             const response = await fetch("localhost:8080/post/new", { method: "POST", body: JSON.stringify(test_post) });
-            expect(!!response);
+            expect(!!response).toBeTrue();
             const result = (await response.json()) as Post;
-            expect(result.id > 0);
-            expect(result.slug == "bun-test-post");
+            expect(result.id).toBeGreaterThan(0);
+            expect(result.slug).toBe("bun-test-post");
             test_post_id = result.id;
         })
         test("duplicate slug", async () => {
             const response = await fetch("localhost:8080/post/new", { method: "POST", body: JSON.stringify({ ...test_post, title: "Duplicated Bun Post" }) })
-            expect(!!response);
-            expect(response.ok == false);
-            expect(response.status >= 400 && response.status < 500); // some 4xxx status code
+            expect(response).toBeTruthy();
+            expect(response.ok).toBeFalse();
+            expect(response.status).toBeGreaterThanOrEqual(500);
         })
         test("update", async () => {
             // we are going to update the content to be "Updated Post Content"
-            const response = await fetch("localhost:8080/post/edit", { method: "PUT", body: JSON.stringify({ ...test_post, content:" Updated Post Content" }) });
-            expect(!!response);
+            const response = await fetch("localhost:8080/post/edit", { method: "PUT", body: JSON.stringify({ ...test_post, content: " Updated Post Content" }) });
+            expect(!!response).toBeTrue();
 
             // then re-fetch the post via id
             const fetch_response = await fetch(`localhost:8080/post/${test_post_id}`, { method: "GET" });
-            expect(!!fetch_response);
+            expect(!!fetch_response).toBeTrue();
             const result = (await fetch_response.json()) as Post;
-            expect(result.id == test_post_id);
-            expect(result.content == "Updated Post Content");            
+            expect(result.id).toBe(test_post_id as number);
+            expect(result.content == "Updated Post Content");
         })
         test("delete", async () => {
             const response = await fetch(`localhost:8080/post/delete/${test_post_id}`, { method: "DELETE" });
-            expect(!!response);
-            expect(response.ok);
+            expect(!!response).toBeTrue();
+            expect(response.ok).toBeTrue();
         })
     }),
         describe("pagination", () => {
-            /** @todo this entire test block */
-            test("mass-creation", () => {
-
+            test("mass-creation", async () => {
+                for (const post of pagination_posts) {
+                    const response = await fetch("localhost:8080/post/new", { method: "POST", body: JSON.stringify(post) });
+                    expect(!!response).toBeTrue();
+                    expect(response.ok).toBeTrue();
+                    const result = (await response.json()) as Post;
+                    expect(result.id).toBeGreaterThan(0);
+                    pagination_ids.set(post.slug, result.id);
+                }
+                expect(pagination_ids.size == pagination_posts.length);
             })
-            test("get", () => {
+            test("get", async () => {
+                const response = await fetch("localhost:8080/posts", { method: "GET" });
+                expect(!!response).toBeTrue();
+                expect(response.ok).toBeTrue();
+                const result = (await response.json()) as PostsResponse;
+                expect(result.posts.length).toBe(10);
+                expect(result.per_page).toBe(10);
+                expect(result.current_page).toBe(1);
+                expect(result.total_pages).toBeGreaterThan(1);
 
+                const page2_response = await fetch("localhost:8080/posts?offset=10", { method: "GET" });
+                expect(!!page2_response).toBeTrue();
+                expect(page2_response.ok).toBeTrue();
+                const page2 = (await page2_response.json()) as PostsResponse;
+                expect(page2.current_page).toBe(2);
+                expect(page2.posts.length).toBe(10);
             })
-            test("get via category", () => {
+            test("get via category", async () => {
+                // there should only be 2 'learning' posts
+                const learning_response = await fetch("localhost:8080/posts/learning", { method: "GET" });
+                expect(!!learning_response).toBeTrue();
+                expect(learning_response.ok).toBeTrue();
+                const learning = (await learning_response.json()) as PostsResponse;
+                expect(learning.total_posts).toBe(2);
+                expect(learning.total_pages).toBe(1);
+                expect(learning.current_page).toBe(1);
 
+                // now we get all 'coding' posts
+                // these should include the child categories of coding
+                const coding_response = await fetch("localhost:8080/posts/coding", { method: "GET" });
+                expect(!!coding_response).toBeTrue();
+                expect(coding_response.ok).toBeTrue();
+                const coding = (await coding_response.json()) as PostsResponse;
+                expect(coding.current_page).toBe(1);
+                expect(coding.total_pages).toBeGreaterThan(1);
+                // check for child categoriers
+                expect(coding.posts.find((p) => p.category == "devlog")).toBeTruthy();
+                expect(coding.posts.find((p) => p.category == "gamedev")).toBeTruthy();
             })
-            afterAll(() => {
-
+            afterAll(async () => {
+                for (const [_, id] of pagination_ids) {
+                    const response = await fetch(`localhost:8080/post/delete/${id}`, { method: "DELETE" });
+                    expect(!!response).toBeTrue();
+                    expect(response.ok).toBeTrue();
+                }
             })
         })
 });
