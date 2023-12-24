9fdb5a74e07efac535d9466a97a7b79ebc9a8484
f0rbit <dev@forbit.dev>
2023-12-24 22:01:42 +1030

    fix tests for categories

--- test/tests/categories.test.ts
+++ test/tests/categories.test.ts
@@ -5,12 +5,24 @@ type Category = {
     parent: string
 }
 
+type CategoryNode = {
+    name: string,
+    children: CategoryNode[]
+}
+
+interface CategoryResponse {
+    graph: CategoryNode,
+    categories: Category[]
+}
+
 test("categories", async () => {
     const response = await fetch("localhost:8080/categories", { method: "GET" });
     expect(response).toBeTruthy();
-    const result = (await response.json()) as Category[];
+    const result = (await response.json()) as CategoryResponse;
     expect(result).toBeTruthy();
-    expect(result.length).toBeGreaterThan(0);
-    expect(result.find((cat) => cat.name == "coding")).toBeTruthy();
-    expect(result.find((cat) => cat.name == "webdev" && cat.parent == "devlog")).toBeTruthy();
+    expect(result.graph).toBeTruthy();
+    expect(result.categories).toBeTruthy();
+    expect(result.categories.length).toBeGreaterThan(0);
+    expect(result.categories.find((cat) => cat.name == "coding")).toBeTruthy();
+    expect(result.categories.find((cat) => cat.name == "webdev" && cat.parent == "devlog")).toBeTruthy();
 });
