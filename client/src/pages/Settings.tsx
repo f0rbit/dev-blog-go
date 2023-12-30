586de1010cf55210e57e5fbc0025dc9695c1b9cd
f0rbit <dev@forbit.dev>
2023-12-30 23:28:09 +1030

    implement creating new tokens

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -52,12 +52,33 @@ function Tokens() {
 
     useEffect(() => {
         (async () => {
-            const response = await fetch(`${API_URL}/auth/tokens`, { method: "GET", credentials: "include" });
+            const response = await fetch(`${API_URL}/tokens`, { method: "GET", credentials: "include" });
             if (!response.ok) throw new Error("Couldn't fetch tokens");
             const result = await response.json();
             setTokens(result);
         })();
     }, []);
+
+    async function save(token: TokenCreation) {
+        const mode = token.id < 0 ? "new" : "edit";
+        const response = await fetch(`${API_URL}/token/${mode}`, { method: mode == "new" ? "POST" : "PUT", credentials: "include", body: JSON.stringify(token) });
+        if (!response.ok) return;
+        const result = await response.json() as AccessKey;
+
+        if (tokens == null) {
+            setTokens([ result ]);
+            return;
+        }
+        if (mode == "new") {
+            setTokens([ ...tokens, result ]);
+            setCreating(false);
+        } else {
+            setTokens(tokens.map((t) => {
+                if (t.id != result.id) return t;
+                return result;
+            }));
+        }
+    }
 
     if (tokens == null) return <section className="page-center"><Oval height={20} width={20} strokeWidth={8} /></section>
 
@@ -65,7 +86,7 @@ function Tokens() {
         <div className="flex-row">
             <Search />
             <input type="text" />
-            <button style={{ marginLeft: "auto"}} onClick={() => setCreating(true)}><Plus /><span>Create</span></button>
+            <button style={{ marginLeft: "auto" }} onClick={() => setCreating(true)}><Plus /><span>Create</span></button>
         </div>
         <div className="token-grid">
             <div>Token</div>
@@ -73,8 +94,8 @@ function Tokens() {
             <div>Note</div>
             <div>Enabled</div>
             <div></div>
-            {tokens.map((t) => <TokenRow token={t} />)}
-            {creating && <TokenRow token={{ ...EMPTY_TOKEN, user_id: user.id }} />}
+            {tokens.map((t) => <TokenRow token={t} save={save} />)}
+            {creating && <TokenRow token={{ ...EMPTY_TOKEN, user_id: user.user_id }} save={save} />}
         </div>
     </div>;
 }
@@ -83,23 +104,24 @@ type TokenCreation = Omit<AccessKey, "created_at" | "updated_at">
 
 const EMPTY_TOKEN: Omit<TokenCreation, "user_id"> = {
     id: -1,
-    value: "",
+    value: "<generated>",
     name: "",
     note: "",
     enabled: true,
 }
 
-function TokenRow({ token }: { token: TokenCreation }) {
+function TokenRow({ token, save }: { token: TokenCreation, save: (token: TokenCreation) => void }) {
     const [editing, setEditing] = useState(token);
     const mode = token.id < 0 ? "create" : "edit";
     const [enabled, setEnabled] = useState(mode == "create");
-    const icon = mode == "create" ? <Check /> : <Save />
+    const [saving, setSaving] = useState(false);
+    const icon = saving ? <Oval width={18} height={18} strokeWidth={8} /> : (mode == "create" ? <Check /> : <Save />);
 
     return <>
-        <input type="text" value={editing.value} onChange={(e) => setEditing({ ...editing, value: e.target.value})} disabled={!enabled}/>
-        <input type="text" value={editing.name} onChange={(e) => setEditing({ ...editing, name: e.target.value })} disabled={!enabled}/>
-        <input type="text" value={editing.note} onChange={(e) => setEditing({ ...editing, note: e.target.value })} disabled={!enabled}/>
-        <input type="checkbox" checked={editing.enabled} onChange={(e) => setEditing({...editing, enabled: e.target.checked})} disabled={!enabled}/>
-        {enabled ? <button onClick={() => {setEnabled(false)}}>{icon}</button> : <button onClick={() => setEnabled(true)}><Edit /></button>}
+        <div className="api-token" style={{ fontFamily: "monospace" }}>{editing.value}</div>
+        <input type="text" value={editing.name} onChange={(e) => setEditing({ ...editing, name: e.target.value })} disabled={!enabled} />
+        <input type="text" value={editing.note} onChange={(e) => setEditing({ ...editing, note: e.target.value })} disabled={!enabled} />
+        <input type="checkbox" checked={editing.enabled} onChange={(e) => setEditing({ ...editing, enabled: e.target.checked })} disabled={!enabled} />
+        {saving ? <button>{icon}</button> : enabled ? <button onClick={() => { setEnabled(false); setSaving(true); save(editing) }}>{icon}</button> : <button onClick={() => setEnabled(true)}><Edit /></button>}
     </>;
 }
