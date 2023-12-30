74332be00dd8e0f99ddde5a871233e5968e48ccf
f0rbit <dev@forbit.dev>
2023-12-30 19:45:14 +1030

    update auth tests to use new token system

--- test/tests/auth.test.ts
+++ test/tests/auth.test.ts
@@ -1,13 +1,14 @@
 import { describe, test, expect } from "bun:test";
+import { AUTH_HEADERS } from "user";
 
 describe("auth tests", () => {
     test("invalid-token fails", async () => {
-        const response = await fetch(`localhost:8080/auth/test?token=123123`, { method: "GET" }); 
+        const response = await fetch(`localhost:8080/auth/user`, { method: "GET", headers: { 'Auth-Token': "rubbish-token" } }); 
         expect(response).toBeTruthy();
         expect(response.ok).toBeFalse();
     });
     test("valid-token succeeds", async () => {
-        const response = await fetch(`localhost:8080/auth/test?token=${process.env.AUTH_TOKEN}`, { method: "GET" });
+        const response = await fetch(`localhost:8080/auth/user`, { method: "GET", headers: AUTH_HEADERS });
         expect(response).toBeTruthy();
         expect(response.ok).toBeTrue();
     });
