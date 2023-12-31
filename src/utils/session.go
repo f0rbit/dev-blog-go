fff6f345393de0cb538a8aa059611e7ef8b47df1
f0rbit <dev@forbit.dev>
2023-12-31 14:25:44 +1030

    CLIENT_URL and COOKIE_DOMAIN env variables

--- src/utils/session.go
+++ src/utils/session.go
@@ -13,10 +13,10 @@ func CreateStore() {
 	store = sessions.NewCookieStore([]byte(secret))
 
 	store.Options = &sessions.Options{
-        Domain:   "localhost",
-		Path:     "/",
-		MaxAge:   3600 * 8, // 8 hours
-		HttpOnly: true,
+		Domain: os.Getenv("COOKIE_DOMAIN"),
+		Path:   "/",
+		MaxAge: 3600 * 8, // 8 hours
+		Secure: true,
 	}
 }
 
